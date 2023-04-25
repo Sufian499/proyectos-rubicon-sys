21e2ce53cd726986b5bdd7c93a402e48c87968ac
Sufian <89130836+Sufian499@users.noreply.github.com>
2023-04-25 16:32:01 +0200

    Update rsyncVserver.sh

--- rsyncVserver.sh
+++ rsyncVserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Comprobación de que el servicio esta activo.
-sh /home/rubi/Vserver/vServer.sh -s /port=690 2> /dev/null # Aquí indicar la ruta del script vServer.sh
+sh /ruta/de/Vserver/vServer.sh -s /port=690 2> /dev/null # Aquí indicar la ruta del script vServer.sh
 
 # Vars
 day=$(date +"%A") # Día de la semana.
@@ -11,8 +11,8 @@ dest_dir="/ruta/destino/${day}_backup" # Importante agregar "${day}" para indica
 host_dest="test@192.168.1.5" # Host de destino.
 
 if [ "$(pgrep vServer)" ] && [ "$day_num" -ne "6" ] && [ "$day_num" -ne "7" ] ;then
-    sh /home/rubi/Vserver/vServer.sh -t /port=690 &> /dev/null # Aquí indicar la ruta del script vServer.sh
+    sh /ruta/de/Vserver/vServer.sh -t /port=690 &> /dev/null # Aquí indicar la ruta del script vServer.sh
     rsync -az "${src_dir}" "${host_dest}":"${dest_dir}" 
-    sh /home/rubi/Vserver/vServer.sh -s /port=690 &> /dev/null			
+    sh /ruta/de/Vserver/vServer.sh -s /port=690 &> /dev/null			
 fi
 exit 0
