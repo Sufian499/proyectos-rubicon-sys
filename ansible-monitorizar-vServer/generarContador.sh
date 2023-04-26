9d8c5abe4a1da5ca0f82a017b34b728f777ec34a
Sufian <89130836+Sufian499@users.noreply.github.com>
2023-04-26 17:29:11 +0200

    Update and rename generarJSON.sh to generarContador.sh

---
+++ ansible-monitorizar-vServer/generarContador.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+fecha_actual=$(date +%F)
+hora_actual=$(date +%T)
+contador=0
+
+echo "$fecha_actual" > datos.txt
+echo "$contador" >> datos.txt
+
+
+
+
--- ansible-monitorizar-vServer/generarJSON.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-fecha_hora_actual=$(date -Iseconds)
-contador=0
-datos="{ \"fecha_hora\": \"$fecha_hora_actual\", \"contador\": $contador }"
-echo $datos > datos.json
-
-
