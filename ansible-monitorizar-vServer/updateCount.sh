7c824125e31df9ad7a9b17d90e0d9e998ee0a50a
Sufian <89130836+Sufian499@users.noreply.github.com>
2023-04-26 17:29:50 +0200

    Update updateCount.sh

--- ansible-monitorizar-vServer/updateCount.sh
+++ ansible-monitorizar-vServer/updateCount.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-fecha_hora_actual=$(date +%s)
-
-
+fecha_actual=$(date +%s)
 contador=0
 
-
-if [ -f "datos.json" ]; then
-   datos=$(cat datos.json)
-   contador=$(echo $datos | jq -r '.contador')
-   fecha_hora_anterior=$(echo $datos | jq -r '.fecha_hora' | date +%s)
+if [ -f "datos.txt" ]; then
+   read fecha_anterior contador_anterior < datos.txt
+   fecha_anterior=$(date -d "$fecha_anterior" +%s)
 else
-   fecha_hora_anterior=$fecha_hora_actual
+   fecha_anterior=$fecha_actual
 fi
 
-
-segundos_transcurridos=$((fecha_hora_actual - fecha_hora_anterior))
-
+segundos_transcurridos=$((fecha_actual - fecha_anterior))
 
 if [ $segundos_transcurridos -gt 3600 ]; then
    contador=0
 else
-	contador=$((contador + 1))
+   contador=$((contador_anterior + 1))
 fi
 
-datos="{ \"fecha_hora\": \"$(date -Iseconds)\", \"contador\": $contador }"
+echo "$(date -Iseconds)" > datos.txt
+echo "$contador" >> datos.txt
 
-
-echo $datos > datos.json
-
-echo "this is the body" | mail -s "this is the subject" "sufianazouz499@gmail.com"
