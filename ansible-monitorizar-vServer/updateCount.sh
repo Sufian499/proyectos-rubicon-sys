6377b76f8dbff3dce4f825ecdef7abf0f7f73760
Sufian <89130836+Sufian499@users.noreply.github.com>
2023-04-27 16:29:44 +0200

    Update updateCount.sh

--- ansible-monitorizar-vServer/updateCount.sh
+++ ansible-monitorizar-vServer/updateCount.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
+# Leer la primera línea del archivo y almacenarla en una variable
+fecha_archivo=$(head -n 1 datos.txt)
 
-fecha_actual=$(date +%s)
-contador=0
+# Obtener la hora actual en el mismo formato que la primera línea del archivo
+fecha_actual=$(date '+%F %T')
 
-if [ -f "datos.txt" ]; then
-   read fecha_anterior contador_anterior < datos.txt
-   fecha_anterior=$(date -d "$fecha_anterior" +%s)
+# Convertir las fechas a timestamp
+fecha_archivo_timestamp=$(date -d "$fecha_archivo" '+%s')
+fecha_actual_timestamp=$(date -d "$fecha_actual" '+%s')
+
+# Calcular la diferencia en segundos entre las dos fechas
+diferencia=$(($fecha_actual_timestamp - $fecha_archivo_timestamp))
+
+# Convertir la diferencia a horas
+diferencia_horas=$(($diferencia / 3600))
+
+# Si la diferencia es mayor o igual a 1 hora, cambiar el contador a cero
+if [ $diferencia_horas -ge 1 ]; then
+  sed -i '2s/.*/0/' datos.txt
 else
-   fecha_anterior=$fecha_actual
-fi
-
-segundos_transcurridos=$((fecha_actual - fecha_anterior))
-
-if [ $segundos_transcurridos -gt 3600 ]; then
-   contador=0
-else
+   contador_anterior=$(sed -n '2p' datos.txt)
    contador=$((contador_anterior + 1))
 fi
 
-echo "$(date -Iseconds)" > datos.txt
+echo "$fecha_actual" > datos.txt
 echo "$contador" >> datos.txt
-
